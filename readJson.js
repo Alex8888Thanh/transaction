7417d59b24c97de8bc63cfefbd39de88512b15e4
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2022-10-22 02:14:40 +0000

    Update readJson.js

--- readJson.js
+++ readJson.js
@@ -1,9 +1,7 @@
 // Loading file system(fs) module
 var fs = require("fs");
-
 // Reading json file asynchronously
 fs.readFile("./input-data.json", function(err, data){
-
 	if(err){ // If error occurred while reading file
 		console.log("Error occured while reading json file");
 	} else {
@@ -11,7 +9,6 @@ fs.readFile("./input-data.json", function(err, data){
 		console.log(jsonObj);
 		console.log()	// new line
 		console.log(typeof jsonObj);
-
 		// Iterating over keys to print the values referred by them
 		for(let key in jsonObj){
 			console.log(key, jsonObj[key]);
