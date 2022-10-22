fde4972f77da17b93325fa9b4433a319b0334ccb
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2022-10-22 02:37:49 +0000

    Update readWriteJson.js

--- readWriteJson.js
+++ readWriteJson.js
@@ -10,28 +10,29 @@ fs.readFile("./input-data.json", function(err, data){
 	
     	var data = {}
     	for(let i = 0;i<jsonObj.length;i++){
-      	let balance = 0
-      	let isValid = false
-      	for(let j = 0;j<jsonObj[i].transformations[j].length;j++){
-            balance+=jsonObj[i].transformations[j].qty*jsonObj[i].transformations[j].size
-      	}
-      	if(balance==0){
-            isValid=true
-      	}
-      	var obj = {
-            jsonObj[i],
-            "balance": balance,
-            "isValid": isValid,
-            "errorReason": null
-      	}
-      	data.push(obj)
-    	let jsonOkObj = [JSON.parse(data)] // without brackets it reverts an error
-    	jsonOkObj.push(data)
-    	let outputData = JSON.stringify(jsonOkObj, null, 2);
-    	fs.writeFile('./output-data.json', outputData, (err) => {
-      	    if (err) throw err;
-      	    console.log('Data written to file');
-    	});
-    	console.log('This is after the write call');
+      	    let balance = 0
+      	    let isValid = false
+      	    for(let j = 0;j<jsonObj[i].transformations[j].length;j++){
+                balance+=jsonObj[i].transformations[j].qty*jsonObj[i].transformations[j].size
+      	    }
+      	    if(balance==0){
+                isValid=true
+      	    }
+      	    var obj = {
+                jsonObj[i],
+                "balance": balance,
+                "isValid": isValid,
+                "errorReason": null
+      	    }
+      	    data.push(obj)
+    	    let jsonOkObj = [JSON.parse(data)] // without brackets it reverts an error
+    	    jsonOkObj.push(data)
+    	    let outputData = JSON.stringify(jsonOkObj, null, 2);
+    	    fs.writeFile('./output-data.json', outputData, (err) => {
+      	        if (err) throw err;
+      	        console.log('Data written to file');
+    	    });
+    	    console.log('This is after the write call');
+        }
     }
 })
