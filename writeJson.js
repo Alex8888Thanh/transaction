9a361dbaa0f6fbd9bff263594a54b1c9a55aa6a4
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2022-10-22 02:13:44 +0000

    Update writeJson.js

--- writeJson.js
+++ writeJson.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 let trans = {
   "transaction": "460a2954c14987bff60478069ea89f2b",
   "transformations": [
@@ -18,12 +17,9 @@ let trans = {
   "isValid": true,
   "errorReason": null
 }
- 
 let data = JSON.stringify(trans, null, 2);
-
 fs.writeFile('./output-data.json', data, (err) => {
     if (err) throw err;
     console.log('Data written to file');
 });
-
 console.log('This is after the write call');
