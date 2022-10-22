ccbe2a6ac7b4201947a291822bfc88c1d8c490d9
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2022-10-22 03:25:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 
+var express = require('express')
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.post('/json', function(request, response){
+    console.log(request.body);      // your JSON
+    var jsonRequest = request.body;
+
+  	  var jsonObj = JSON.parse(jsonRequest);
+	   	var data = {}
+    	for(let i = 0;i<jsonObj.length;i++){
+      	    let balance = 0
+      	    let isValid = false
+      	    for(let j = 0;j<jsonObj[i].transformations[j].length;j++){
+                balance+=jsonObj[i].transformations[j].qty*jsonObj[i].transformations[j].size
+      	    }
+      	    if(balance==0){
+                isValid=true
+      	    }
+      	    var obj = {
+                jsonObj[i],
+                "balance": balance,
+                "isValid": isValid,
+                "errorReason": null
+      	    }
+      	    data.push(obj)
+      }
+    	let jsonOkObj = [JSON.parse(data)] // without brackets it reverts an error
+    	jsonOkObj.push(data)
+    	let outputData = JSON.stringify(jsonOkObj, null, 2);
+
+    response.send(outputData);
+});
+app.listen(3000);
